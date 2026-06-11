fe3a8e2cc3cb2345a41258f94d75ba9d022f6bfa
sudharsanmg2005 <sudharsan.mg2023cse@sece.ac.in>
2026-06-11 10:15:18 +0530

    final commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.PROD ? '/api' : 'http://localhost:5000/api')
+  baseURL: import.meta.env.VITE_API_URL || (import.meta.env.PROD ? '/api' : 'https://crusher-accounting.onrender.com/api')
 });
 
 api.interceptors.request.use((config) => {
